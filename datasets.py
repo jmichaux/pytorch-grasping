48ed02c8c30bcca4ea9448d82712efb805bb9ba0
Jonathan Michaux <jmichaux@ttic.edu>
2018-03-14 17:17:42 -0500

    made a few changes

--- datasets.py
+++ datasets.py
@@ -1,27 +1,39 @@
-import os
-import glob
 import random
-
+from os.path import basename, dirname, join
+import glob
+import numbers
 import numpy as np
 import pandas as pd
 
 from PIL import Image
-from torch.utils.data.dataset import Dataset
+import matplotlib.pyplot as plt
 from scipy.misc import bytescale
+
+import torch
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision import models, utils
+import torchvision.transforms as t
+from torchvision.transforms import functional as f
 
 
 
 class CornellGraspingDataset(Dataset):
-    def __init__(self, csv_file, root_dir, use_depth=True, shuffle_channels=True, sixd_grasp=True, transform=None):
+    def __init__(self, csv_file, root_dir, 
+                 use_depth=False,
+                 concat_depth=False,
+                 sixd_grasp=False, 
+                 transform=None, 
+                 co_transform=None):
         self.df = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
-        self.shuffle_channels = shuffle_channels
+        self.co_transform = co_transform
         self.use_depth = use_depth
-        # image dimensions
-        self.height = 480
-        self.width = 640
-        # number of bounding box points
+        self.concat_depth = concat_depth
+        self.im_height = 480
+        self.im_width = 640
         self.nbox_pts = 4
         self.sixd_grasp = sixd_grasp
         
@@ -46,58 +58,72 @@ class CornellGraspingDataset(Dataset):
                                    self.df.iloc[index, 3])
         
         # open image
-        img = np.array(Image.open(img_name))
+        img = Image.open(img_name)
         
-        if self.shuffle_channels:
-            img = img[:, :, np.random.permutation(3)]
-                    
-        # open target
-        _targets = np.loadtxt(pos_name)
-        num_grasps = int(len(_targets) / 4)
-        idx = random.randint(0, (num_grasps - 1))
-        target = _targets[self.nbox_pts * idx : self.nbox_pts * (idx + 1)]
-        target = target[: , [1, 0]]
-
+        # transform
+        if self.transform:
+            img = self.transform(img)
+        
         # open depth map
+        # TODO: speed this part up
         if self.use_depth:
             _pcd = np.loadtxt(pcd_name, skiprows=10, usecols=(4, 2))
             
-            # normalize pcd_data
-            # TODO: replace bytescale
+            # normalize pcd_data (TODO: replace bytescale)
             _pcd[:, 1] = bytescale(_pcd[:, 1])
             
             # convert pcd to array
             # row = np.floor(index / 640) 
             # col = np.mod(index, 640) 
-            pcd = np.zeros((self.height * self.width))
+            pcd = np.zeros((self.im_height * self.im_width))
             pcd[_pcd[:, 0].astype(int)] = _pcd[:, 1]
             pcd = np.reshape(pcd, (-1, self.width))
+            pcd = Image.fromarray(pcd.astype("uint8"))
+        else:
+            pcd = None
             
-            # replace last channel with pcd
-            img[:, :, 2] = pcd
             
-        # transform
-        if self.transform:
-            img, target = self.transform(img, target)
-            img = self.transform(img)
+        # load random bounding box
+        pos = np.loadtxt(pos_name)
+        num_grasps = int(len(pos) / 4)
+        idx = random.randint(0, (num_grasps - 1))
+        bbox = pos[self.nbox_pts * idx : self.nbox_pts * (idx + 1)]
         
-        # original target has [col, row] cartesion coordinates
-        # transformed target has [row, col] numpy array coordinates
-        y1, x1 = target[0]
-        y2, x2 = target[1]
-        y3, x3 = target[2]
-        y4, x4 = target[3]
+            
+        # co_transforms
+        if self.co_transform:
+            img, bbox, pcd = self.co_transform(img, bbox, pcd)
         
-        # calculate x, y, h, w, theta of bounding box
+        
+        # img and pcd
+        if self.use_depth:
+            if self.concat_depth:
+                img = torch.cat((img, pcd), 0)
+            else:
+                # replace last channel with pcd
+                img = torch.cat((img[:2, :, :], pcd), 0)
+   
+
+        # corners of bbox
+        x1, y1 = bbox[0]
+        x2, y2 = bbox[1]
+        x3, y3 = bbox[2]
+        x4, y4 = bbox[3]
+        
+        # center of bbox
         x = (x1 + x3) / 2
         y = (y1 + y3) / 2
-        h = np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-        w = np.sqrt((x1 - x4)**2 + (y1 - y4)**2)
-        theta = np.arctan((y1 - y2) / (x1 - x2))
+        
+        # bbox height and width
+        box_h = np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        box_w = np.sqrt((x1 - x4)**2 + (y1 - y4)**2)
+        
+        # orientation of bbox
+        theta = np.arctan((y2 - y1) / (x2 - x1))
         
         if self.sixd_grasp:
-            target = np.array([x, y, h, w, np.cos(2*theta), np.sin(2*theta)])
+            target = np.array([x, y, h_box, w_box, np.cos(2*theta), np.sin(2*theta)])
         else:
-            target = np.array([x, y, h, w, theta])
+            target = np.array([x, y, h_box, w_box, theta])
 
-        return img, target
+        return img, target, bbox
